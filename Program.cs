2370b9afa376034d880205bbd910abb532d8ca2d
mircavid001 <mirgavidmiriyev@gmail.com>
2024-05-21 10:03:24 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+//void topla()
+//{
+//    Console.WriteLine("plise enter first number");
+//    int num1 = int.Parse(Console.ReadLine());
+//    Console.WriteLine("plase enter second number");
+//    int num2 = int.Parse(Console.ReadLine());
+//    Console.WriteLine(num1 + num2);
+//}
+
+//topla();
+
+//void topla(int num1, int num2)
+//{
+//    int res = num1 + num2;
+//    Console.WriteLine(res);
+//}
+
+//topla(16, 28);
+
+
+
+int topla(int num1, int num2)
+{
+    int res = num1 + num2;
+    return res;
+}
